4d47d30a04fe3b3019e7107978c30d151779c2c4
MOHAMMAD Aowis <oves7860@gmail.com>
2024-09-19 14:37:28 +0200

    changement du fichier de connexion base de données de local à production

--- config/connexion_bd.php
+++ config/connexion_bd.php
@@ -1,11 +1,12 @@
 <?php
-$host = 'localhost'; // Adresse du serveur de base de données
-$db = 'arcadiaZoo'; // Nom de la base de données
-$user = 'root'; // Nom d'utilisateur
-$pass = ''; // Mot de passe
+$host = 'f80b6byii2vwv8cx.chr7pe7iynqr.eu-west-1.rds.amazonaws.com'; // Adresse du serveur de base de données
+$db = 'yfnkm4lvqps5djol'; // Nom de la base de données
+$user = 'wpuag6h82b11m6i7'; // Nom d'utilisateur
+$pass = 'x0m4zf3b597abg9u'; // Mot de passe
+$port = 3306; // Port de la base de données
 
 // DSN (Data Source Name)
-$dsn = "mysql:host=$host;dbname=$db;charset=utf8mb4";
+$dsn = "mysql:host=$host;port=$port;dbname=$db;charset=utf8mb4";
 
 try {
     // Création d'une instance PDO
@@ -17,3 +18,4 @@ try {
     die("Erreur de connexion à la base de données : " . $e->getMessage());
 }
 ?>
+
--- mongodb_connexion.php
+++
@@ -1,9 +0,0 @@
-<?php
-require 'vendor/autoload.php'; // si vous avez utilisé Composer
-
-use MongoDB\Client;
-
-$mongoClient = new Client("mongodb://localhost:27017");
-$database = $mongoClient->ArcadiaZoo;
-$collection = $database->animal_views;
-?>
