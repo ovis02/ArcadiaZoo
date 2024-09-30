605accce3be22fe6f1475aa43e83822f501c2c82
MOHAMMAD Aowis <oves7860@gmail.com>
2024-09-30 23:58:56 +0200

    test

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const uri =
   "mongodb+srv://oves7860:Mohammad786.@arcadiazoo.dheoc.mongodb.net/ArcadiaZoo?retryWrites=true&w=majority";
 const client = new MongoClient(uri);
 
-// Middleware pour gérer les erreurs
+// Middleware pour gérer les erreurs CORS
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
@@ -23,27 +23,32 @@ app.use((req, res, next) => {
   next();
 });
 
-app.get("/", async (req, res) => {
-  try {
-    await client.connect();
+// Démarre le client MongoDB
+client
+  .connect()
+  .then(() => {
     console.log("Connecté à MongoDB!");
 
+    // Démarre le serveur
+    app.listen(port, () => {
+      console.log(`Le serveur est en écoute sur le port ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Erreur de connexion à MongoDB :", err);
+  });
+
+// Route GET pour récupérer tous les animaux
+app.get("/animals", async (req, res) => {
+  try {
     const database = client.db("ArcadiaZoo");
     const collection = database.collection("animalviews");
 
-    // Exemple d'opération : Récupérer tous les animaux
+    // Récupérer tous les animaux
     const animals = await collection.find().toArray();
-
     res.status(200).json(animals); // Retourne les animaux en format JSON
   } catch (err) {
-    console.error("Erreur de connexion :", err);
-    res.status(500).send("Erreur de connexion à la base de données");
-  } finally {
-    await client.close(); // Ferme la connexion après les opérations
+    console.error("Erreur lors de la récupération des animaux :", err);
+    res.status(500).send("Erreur de récupération des animaux : " + err.message);
   }
-});
-
-// Démarre le serveur
-app.listen(port, () => {
-  console.log(`Le serveur est en écoute sur le port ${port}`);
 });
